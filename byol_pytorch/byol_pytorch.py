73d408720f373e06ced2b7c60f6b641e92b5f877
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-07-09 14:55:00 +0200

    Removed kornia to add support for PyTorch < 1.5.0

--- byol_pytorch/byol_pytorch.py
+++ byol_pytorch/byol_pytorch.py
@@ -6,16 +6,16 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-from kornia import augmentation as augs
-from kornia import filters, color
-
 # helper functions
+
 
 def default(val, def_val):
     return def_val if val is None else val
 
+
 def flatten(t):
     return t.reshape(t.shape[0], -1)
+
 
 def singleton(cache_key):
     def inner_fn(fn):
@@ -28,31 +28,40 @@ def singleton(cache_key):
             instance = fn(self, *args, **kwargs)
             setattr(self, cache_key, instance)
             return instance
+
         return wrapper
+
     return inner_fn
 
+
 # loss fn
+
 
 def loss_fn(x, y):
     x = F.normalize(x, dim=-1, p=2)
     y = F.normalize(y, dim=-1, p=2)
     return 2 - 2 * (x * y).sum(dim=-1)
 
+
 # augmentation utils
+
 
 class RandomApply(nn.Module):
     def __init__(self, fn, p):
         super().__init__()
         self.fn = fn
         self.p = p
+
     def forward(self, x):
         if random.random() > self.p:
             return x
         return self.fn(x)
 
+
 # exponential moving average
 
-class EMA():
+
+class EMA:
     def __init__(self, beta):
         super().__init__()
         self.beta = beta
@@ -62,32 +71,39 @@ class EMA():
             return new
         return old * self.beta + (1 - self.beta) * new
 
+
 def update_moving_average(ema_updater, ma_model, current_model):
-    for current_params, ma_params in zip(current_model.parameters(), ma_model.parameters()):
+    for current_params, ma_params in zip(
+        current_model.parameters(), ma_model.parameters()
+    ):
         old_weight, up_weight = ma_params.data, current_params.data
         ma_params.data = ema_updater.update_average(old_weight, up_weight)
 
+
 # MLP class for projector and predictor
 
+
 class MLP(nn.Module):
-    def __init__(self, dim, projection_size, hidden_size = 4096):
+    def __init__(self, dim, projection_size, hidden_size=4096):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(dim, hidden_size),
             nn.BatchNorm1d(hidden_size),
             nn.ReLU(inplace=True),
-            nn.Linear(hidden_size, projection_size)
+            nn.Linear(hidden_size, projection_size),
         )
 
     def forward(self, x):
         return self.net(x)
 
+
 # a wrapper class for the base neural network
 # will manage the interception of the hidden layer output
 # and pipe it into the projecter and predictor nets
 
+
 class NetWrapper(nn.Module):
-    def __init__(self, net, projection_size, projection_hidden_size, layer = -2):
+    def __init__(self, net, projection_size, projection_hidden_size, layer=-2):
         super().__init__()
         self.net = net
         self.layer = layer
@@ -113,11 +129,11 @@ class NetWrapper(nn.Module):
 
     def _register_hook(self):
         layer = self._find_layer()
-        assert layer is not None, f'hidden layer ({self.layer}) not found'
+        assert layer is not None, f"hidden layer ({self.layer}) not found"
         handle = layer.register_forward_hook(self._hook)
         self.hook_registered = True
 
-    @singleton('projector')
+    @singleton("projector")
     def _get_projector(self, hidden):
         _, dim = hidden.shape
         projector = MLP(dim, self.projection_size, self.projection_hidden_size)
@@ -133,7 +149,7 @@ class NetWrapper(nn.Module):
         _ = self.net(x)
         hidden = self.hidden
         self.hidden = None
-        assert hidden is not None, f'hidden layer {self.layer} never emitted an output'
+        assert hidden is not None, f"hidden layer {self.layer} never emitted an output"
         return hidden
 
     def forward(self, x):
@@ -142,35 +158,37 @@ class NetWrapper(nn.Module):
         projection = projector(representation)
         return projection
 
+
 # main class
 
+
 class BYOL(nn.Module):
-    def __init__(self, net, image_size, hidden_layer = -2, projection_size = 256, projection_hidden_size = 4096, augment_fn = None, moving_average_decay = 0.99):
+    def __init__(
+        self,
+        net,
+        image_size,
+        hidden_layer=-2,
+        projection_size=256,
+        projection_hidden_size=4096,
+        augment_fn=None,
+        moving_average_decay=0.99,
+    ):
         super().__init__()
 
-        # default SimCLR augmentation
-
-        DEFAULT_AUG = nn.Sequential(
-            RandomApply(augs.ColorJitter(0.8, 0.8, 0.8, 0.2), p=0.8),
-            augs.RandomGrayscale(p=0.2),
-            augs.RandomHorizontalFlip(),
-            RandomApply(filters.GaussianBlur2d((3, 3), (1.5, 1.5)), p=0.1),
-            augs.RandomResizedCrop((image_size, image_size)),
-            color.Normalize(mean=torch.tensor([0.485, 0.456, 0.406]), std=torch.tensor([0.229, 0.224, 0.225]))
+        self.online_encoder = NetWrapper(
+            net, projection_size, projection_hidden_size, layer=hidden_layer
         )
-
-        self.augment = default(augment_fn, DEFAULT_AUG)
-
-        self.online_encoder = NetWrapper(net, projection_size, projection_hidden_size, layer=hidden_layer)
         self.target_encoder = None
         self.target_ema_updater = EMA(moving_average_decay)
 
-        self.online_predictor = MLP(projection_size, projection_size, projection_hidden_size)
+        self.online_predictor = MLP(
+            projection_size, projection_size, projection_hidden_size
+        )
 
         # send a mock image tensor to instantiate singleton parameters
         self.forward(torch.randn(2, 3, image_size, image_size))
 
-    @singleton('target_encoder')
+    @singleton("target_encoder")
     def _get_target_encoder(self):
         target_encoder = copy.deepcopy(self.online_encoder)
         return target_encoder
@@ -180,12 +198,14 @@ class BYOL(nn.Module):
         self.target_encoder = None
 
     def update_moving_average(self):
-        assert self.target_encoder is not None, 'target encoder has not been created yet'
-        update_moving_average(self.target_ema_updater, self.target_encoder, self.online_encoder)
+        assert (
+            self.target_encoder is not None
+        ), "target encoder has not been created yet"
+        update_moving_average(
+            self.target_ema_updater, self.target_encoder, self.online_encoder
+        )
 
-    def forward(self, x):
-        image_one, image_two = self.augment(x), self.augment(x)
-
+    def forward(self, image_one, image_two):
         online_proj_one = self.online_encoder(image_one)
         online_proj_two = self.online_encoder(image_two)
 
